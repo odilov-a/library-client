b0a57ec34dcb2ee737713fb7130e25be426f3e8e
odilov-a <akbarjonodilov01@gmail.com>
2024-04-25 11:56:20 +0500

    Update UI text and styling in index.html, login.html, and authors.html

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, globalShortcut } = require("electron");
 const path = require("path");
 let mainWindow;
 
@@ -8,15 +8,26 @@ function createWindow() {
     height: 800,
     webPreferences: {
       nodeIntegration: true,
+      devTools: false,
     },
   });
-  mainWindow.loadFile(path.join(__dirname, "index.html")); 
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
   mainWindow.on("closed", function () {
     mainWindow = null;
   });
+  mainWindow.setMenu(null);
+  globalShortcut.register("CommandOrControl+F", () => {
+    mainWindow.setFullScreen(!mainWindow.isFullScreen());
+  });
 }
 
-app.on("ready", createWindow);
+app.on("ready", () => {
+  createWindow();
+  app.on("will-quit", () => {
+    globalShortcut.unregisterAll();
+  });
+});
+
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") {
     app.quit();
